1180622bd2f2f985bd46d657e230978cb4333d2c
Pasindu Madusanka <pasindusenerath@gmail.com>
2018-10-31 08:22:50 +0530

    Create testsql.php

---
+++ testsql.php
@@ -0,0 +1,12 @@
+<?php
+
+$mysqli = new mysqli('127.0.0.1', 'root', '', '');
+
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') '
+            . $mysqli->connect_error);
+}
+echo '<p>Connection OK '. $mysqli->host_info.'</p>';
+echo '<p>Server '.$mysqli->server_info.'</p>';
+$mysqli->close();
+?>
